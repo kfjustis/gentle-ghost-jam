ff6e1d0731be0ecd864a31ff66e524a68c637f89
kfjustis <kfjustis@gmail.com>
2019-10-19 18:35:16 -0500

    Player animation tweaks and added custom font

--- global.gd
+++ global.gd
@@ -1,9 +1,12 @@
 extends Node
 
 func _ready():
-	pass
+	OS.set_window_always_on_top(true);
 
 # warning-ignore:unused_argument
 func _process(delta):
 	if Input.is_action_pressed("ui_cancel"):
 		get_tree().quit()
+
+	if Input.is_action_just_released("toggle_fullscreen"):
+			OS.window_fullscreen = !OS.window_fullscreen;
--- n2d_player.gd
+++ n2d_player.gd
@@ -3,9 +3,10 @@ extends KinematicBody2D;
 var player_sprite:  Sprite;
 var player_aplayer: AnimationPlayer;
 
-var has_control = true;
-var speed       = 180;
-var velocity    = Vector2();
+var has_control: bool    = true;
+var frame_index: int     = 0;
+var speed      : float   = 180.0;
+var velocity   : Vector2 = Vector2();
 
 func _ready():
 	player_sprite = get_node("./Sprite");
@@ -13,15 +14,32 @@ func _ready():
 
 # warning-ignore:unused_argument
 func _process(delta):
+	if (Input.is_action_just_pressed("player_left")  ||
+		Input.is_action_just_pressed("player_right") ||
+		Input.is_action_just_pressed("player_up")    ||
+		Input.is_action_just_pressed("player_down")):
+			# Tapping movement keys should still change the
+			# player sprite a little bit, so advance frame
+			# by one.
+			frame_index += 1;
+			if frame_index > (player_sprite.get_hframes() - 1):
+				frame_index = 0;
+			player_sprite.set_frame(frame_index);
+
 	if velocity.x > 0:
 		player_sprite.set_flip_h(false);
 		
 	if velocity.x < 0:
 		player_sprite.set_flip_h(true);
 		
-	if (velocity.x == 0 && velocity.y == 0):
+	if velocity.x == 0 && velocity.y == 0:
 		player_aplayer.stop();
+		# .stop() resets to frame 0 which is boring.
+		player_sprite.set_frame(frame_index);
 	else:
+		# Always play animation from frame 0
+		if !player_aplayer.is_playing():
+			player_sprite.set_frame(0);
 		player_aplayer.play("walk_right");
 
 # warning-ignore:unused_argument
